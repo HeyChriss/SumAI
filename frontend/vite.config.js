abc2de027671c2d66372ceab98c71fba03757cc6
rawwyurr <roger18gm@gmail.com>
2025-02-28 23:04:12 -0700

    html elements

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   // root: "src/",
   plugins: [
-    tailwindcss(),
+    tailwindcss()
   ],
   build: {
     rollupOptions: {
